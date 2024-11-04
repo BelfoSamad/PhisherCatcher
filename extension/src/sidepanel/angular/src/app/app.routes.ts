7de60270d95bd9eb124459e19c7da8a117f52147
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-11-04 15:32:26 +0100

    feat(ext): setup sidepanel's base project (basic ui and components)

--- extension/src/sidepanel/angular/src/app/app.routes.ts
+++ extension/src/sidepanel/angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {RegisterComponent} from './auth/register/register.component';
+import {LoginComponent} from './auth/login/login.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Register'
+    },
+];
