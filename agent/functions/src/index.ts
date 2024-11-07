df39f57df48ec1d34b9b0e171a7812f80e4799cc
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-11-07 12:23:07 +0100

    feat(agent): setup project's structure

--- agent/functions/src/index.ts
+++ agent/functions/src/index.ts
@@ -1,74 +1,82 @@
 import * as z from "zod";
-
-// Import the Genkit core libraries and plugins.
-import {generate} from "@genkit-ai/ai";
 import {configureGenkit} from "@genkit-ai/core";
 import {firebase} from "@genkit-ai/firebase";
 import {googleAI} from "@genkit-ai/googleai";
-
-// Import models from the Google AI plugin. The Google AI API provides access to
-// several generative models. Here, we import Gemini 1.5 Flash.
-import {gemini15Flash} from "@genkit-ai/googleai";
-
-// From the Firebase plugin, import the functions needed to deploy flows using
-// Cloud Functions.
+import {defineSecret} from "firebase-functions/params";
+import {applicationDefault, initializeApp} from "firebase-admin/app";
 import {firebaseAuth} from "@genkit-ai/firebase/auth";
 import {onFlow} from "@genkit-ai/firebase/functions";
+import {credential} from "firebase-admin";
+import {config} from "dotenv";
+import {getFirestore} from "firebase-admin/firestore";
+import {dotprompt} from "@genkit-ai/dotprompt";
+import {defineFlow} from "@genkit-ai/flow";
+
+// Keys
+const googleAIapiKey = defineSecret("GOOGLE_GENAI_API_KEY");
+const whoisApiKey = defineSecret("WHOIS_API_KEY");
  
+// ----------------------------------------- Initializations
+const debug = true;
+const app = initializeApp({credential: debug ? credential.cert("./phishercatcher-creds.json") : applicationDefault()});
+const firestore = getFirestore(app);
+if (debug) {
+  firestore.settings({
+    host: "localhost",
+    port: 8080,
+    ssl: false,
+  });
+}
+
+
+// ----------------------------------------- Configurations
+if (debug) config();
 configureGenkit({
   plugins: [
-    // Load the Firebase plugin, which provides integrations with several
-    // Firebase services.
-    firebase(),
-    // Load the Google AI plugin. You can optionally specify your API key
-    // by passing in a config object; if you don't, the Google AI plugin uses
-    // the value from the GOOGLE_GENAI_API_KEY environment variable, which is
-    // the recommended practice.
-    googleAI(),
+    debug ? firebase({projectId: "phishercatcher-53f64"}) : firebase(),
+    dotprompt(),
+    debug ? googleAI({apiKey: process.env.GEMINI_API_KEY}) : googleAI(),
   ],
-  // Log debug output to tbe console.
-  logLevel: "debug",
-  // Perform OpenTelemetry instrumentation and enable trace collection.
+  logLevel: debug ? "debug" : "info",
   enableTracingAndMetrics: true,
 });
 
-// Define a simple flow that prompts an LLM to generate menu suggestions.
-export const menuSuggestionFlow = onFlow(
+// ----------------------------------------- Flows
+export const analyzeWebsiteFlow = debug ? defineFlow(
   {
-    name: "menuSuggestionFlow",
-    inputSchema: z.string(),
-    outputSchema: z.string(),
-    authPolicy: firebaseAuth((user) => {
-      // By default, the firebaseAuth policy requires that all requests have an
-      // `Authorization: Bearer` header containing the user's Firebase
-      // Authentication ID token. All other requests are rejected with error
-      // 403. If your app client uses the Cloud Functions for Firebase callable
-      // functions feature, the library automatically attaches this header to
-      // requests.
-
-      // You should also set additional policy requirements as appropriate for
-      // your app. For example:
-      // if (!user.email_verified) {
-      //   throw new Error("Verified email required to run flow");
-      // }
+    name: "analyzeWebsiteFlow",
+    inputSchema: z.object({
+      userId: z.string(),
+      url: z.string(),
+    }),
+    outputSchema: z.object({
+      decision: z.number(),
+      reasons: z.array(z.string()),
     }),
   },
-  async (subject) => {
-		// Construct a request and send it to the model API.
-    const prompt =
-      `Suggest an item for the menu of a ${subject} themed restaurant`;
-    const llmResponse = await generate({
-      model: gemini15Flash,
-      prompt: prompt,
-      config: {
-        temperature: 1,
-      },
-    });
-
-		// Handle the response from the model API. In this sample, we just
-		// convert it to a string, but more complicated flows might coerce the
-		// response into structured output or chain the response into another
-		// LLM call, etc.
-    return llmResponse.text();
-  }
+  doAnalyzeWebsiteFlow,
+) : onFlow(
+  {
+    name: "analyzeWebsiteFlow",
+    httpsOptions: {
+      secrets: [googleAIapiKey],
+      cors: true,
+    },
+    inputSchema: z.object({
+      userId: z.string(),
+      url: z.string(),
+    }),
+    outputSchema: z.object({
+      decision: z.number(),
+      reasons: z.array(z.string()),
+    }),
+    authPolicy: firebaseAuth((user) => {
+      if (!user) throw Error("ERROR::AUTH");
+    }),
+  },
+  doAnalyzeWebsiteFlow,
 );
+
+
+async function doAnalyzeWebsiteFlow(input: any): Promise<any> {
+}
