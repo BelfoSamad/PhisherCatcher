03a8c7a8ae132e2fbfb62c2fcf95918d59774d44
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-11-22 14:23:31 +0100

    fix: update .gitignore and remove firebase_configs

--- extension/src/firebase_configs.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDoA3JmNA6PA4GrqwGX3dSfkH170C-gyQ8",
-    authDomain: "phishercatcher-53f64.firebaseapp.com",
-    projectId: "phishercatcher-53f64",
-    storageBucket: "phishercatcher-53f64.firebasestorage.app",
-    messagingSenderId: "175915771245",
-    appId: "1:175915771245:web:5e297ba3e3af545c71e664",
-    measurementId: "G-K0TDER4073"
-};
