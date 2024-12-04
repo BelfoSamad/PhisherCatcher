dd2f2757434566e4cb4523640ae16b5c81b753f3
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-12-04 09:21:51 +0100

    feat: build website

--- agent/functions/src/tools/firestore.ts
+++
@@ -1,6 +0,0 @@
-import {Firestore} from "firebase-admin/firestore";
-
-// ----------------------------------------- Firestore Utilities
-export async function addWebsite(firestore: Firestore, domain: string, analysis: any) {
-    firestore.collection("websites").doc(domain).create(analysis);
-}
--- extension/src/configs.js
+++
@@ -1,9 +0,0 @@
-import {initializeApp} from 'firebase/app';
-import {firebaseConfig} from './firebase_configs';
-
-const app = initializeApp(firebaseConfig);
-export {app}
-export const defaults = {
-  enableAutoBlock: true, // auto block website after check
-  enableForceBlock: true, // allow only when legit
-}
